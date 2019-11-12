ba4f17f3ba0f9227b22a145470bbc22c9241e523
Khoirur Rozaq <roozsec@gmail.com>
2019-11-12 23:24:02 +0700

    add database.sql

---
+++ README.md
@@ -0,0 +1,19 @@
+# RestAPI CRUD Golang
+
+## CRUD
+- Create
+- Read
+- Update
+- Delete
+
+## Instalasi
+1. Clone repositori ini
+2. Install package
+```
+go get github.com/go-sql-driver/mysql
+go get github.com/gorilla/mux
+```
+3. Jalankan `go build`
+4. Lalu `./golang`
+5. Sesuaikan konfigurasi database pada file `mysql.go`
+6. Done
---
+++ amikom.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 12, 2019 at 11:13 PM
+-- Server version: 5.7.27-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amikom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `npm` char(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jk` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `npm`, `nama`, `jk`) VALUES
+(1, '17.02.0152', 'Khoirur Roza', 'Perempuan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
